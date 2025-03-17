31adb7dc599c89a7ffdaa96dc88f8992ec8ef750
CHRISTIAN EARL A. BONA <2202546@cc.usls.edu>
2025-03-17 15:30:12 +0800

    Bona_MidtermExam

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FeatureController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/BonaChristianEarl', 'App\Http\Controllers\FeatureController@Index');
+Route::get('/BonaChristianEarl', [FeatureController::class, 'index']);
 
 
 
